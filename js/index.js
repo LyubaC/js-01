3dfeff82ae2739dccea425b4d66bc3ef016ad4e4
Lyubov Chepil <lyubac@ukr.net>
2018-07-30 16:20:01 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,27 @@
+//const hello = "Hello World";
+//alert(hello);
+
+const username = prompt ("Enter your name");
+const greetings ="Hello," + username;
+alert(greetings);
+
+const userAge = parseInt(prompt('Enter your age'));
+alert('Your age is: ' + userAge);
+
+const nextAge = userAge + 1;
+alert('Next year you will be: ' + nextAge);
+
+if (userAge >=18) {
+    alert("You are adult");
+}
+    else {
+        alert("You are child");
+    }
+function add(a, b, c) {
+    const result = a + b + c;
+    return result;
+}
+const result = add(2,2,2);
+alert(result);
+
+alert(Math.sqrt(4));
